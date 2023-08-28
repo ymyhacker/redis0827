e328bfe5b655c0a23127e9b011e8a5bef96f9ce7
YmY <98162610+ymyhacker@users.noreply.github.com>
2023-08-28 21:53:03 +0800

    Update commands.go

--- commands/commands.go
+++ commands/commands.go
@@ -4,12 +4,9 @@ import (
 	"strconv"
 	"time"
 	"fmt"
+	"strings"
 	// "github.com/ymyhacker/redis0827/tree/YmY-branch/db"
 )
-
-type DatabaseManager struct {
-	Database *Database
-}
 
 
 type CommandResponse struct {
@@ -92,23 +89,22 @@ func (db *Database) ListKeys() []string {
 }
 
 
-func NewDatabaseManager() *DatabaseManager {
-	return &DatabaseManager{
-		Database: NewDatabase(),
-	}
-}
 
 
-func ExecuteCommand(dm *DatabaseManager, command string, args []string) CommandResponse {
-	switch command {
-	case "SET":{
+func ExecuteCommand(dm *Database, command string, args []string) CommandResponse {
+	
+	fmt.Println(command)
+	fmt.Println(args)
+	switch strings.ToLower(command) {
+		
+	case "set":{
 		if len(args) != 2 {
 			return CommandResponse{Message: "Usage: SET key value expiretime"}
 		}
-		dm.Database.Set(args[0], args[1])
+		dm.Set(args[0], args[1])
 		return CommandResponse{Message: "OK"}
 		}// ... (other cases)
-	case "EXPIRE":{
+	case "expire":{
 		if len(args) != 2 {
 			return CommandResponse{Message: "Usage: EXPIRE key ttl"}
 		}
@@ -118,40 +114,40 @@ func ExecuteCommand(dm *DatabaseManager, command string, args []string) CommandR
 				return CommandResponse{Message: "Invalid TTL value"}
 			}
 
-			response := dm.Database.Expire(args[0], ttl)
+			response := dm.Expire(args[0], ttl)
 			return response
 
 	}	
 
-	case "DELETE":
+	case "delete":
 		{
 		if len(args) != 1 {
 			return CommandResponse{Message: "Usage: DELETE key"}
 		}
-		dm.Database.Delete(args[0])
+		dm.Delete(args[0])
 		return CommandResponse{Message: "OK"}
 	}
-	case "GET":
+	case "get":
 		{
 		if len(args) != 1 {
 			return CommandResponse{Message: "Usage: GET key"}
 		}
-		value,_ := dm.Database.Get(args[0])
+		value,_ := dm.Get(args[0])
 		return CommandResponse{Message: value}
 		// ... (other cases)
 		}
-	case "Exist":
+	case "exist":
 		if len(args) != 1 {
 			return CommandResponse{Message: "Usage: EXISTS key"}
 		}
 		key := args[0]
-		if dm.Database.KeyExists(key) {
+		if dm.KeyExists(key) {
 			return CommandResponse{Message: fmt.Sprintf("Key %s exists", key)}
 		}
 		return CommandResponse{Message: fmt.Sprintf("Key %s does not exist", key)}
-	case "LIST":
+	case "list":
 		// keys := dm.ListKeys()
-		keys := dm.Database.ListKeys()
+		keys := dm.ListKeys()
 		return CommandResponse{Message: fmt.Sprintf("Keys in the database: %v", keys)}
 	default:
 		return CommandResponse{Message: "Unknown command"}
