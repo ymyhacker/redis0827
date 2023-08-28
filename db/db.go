b96077b752a90eb228431b47e43df17ddf95cd6a
YmY <98162610+ymyhacker@users.noreply.github.com>
2023-08-28 20:14:40 +0800

    Update db.go

--- db/db.go
+++ db/db.go
@@ -2,16 +2,30 @@ package db
 
 import (
 	   "time"
-	   "strings"
+	//    "strings"
 )
+// type Database struct {
+//     data     map[string]string
+//     expiresAt map[string]time.Duration
+// }
+
+
+// func NewDatabase() *Database {
+// 	return &Database{
+// 		data: make(map[string]string),
+// 		expiresAt: make(map[string]time.Duration),
+// 	}
+// }
+
+
 type Database struct {
-    data     map[string]string
-    expiresAt map[string]time.Time
+	data        map[string]string
+	expirations map[string]time.Time
 }
 
 func NewDatabase() *Database {
 	return &Database{
-		data: make(map[string]string),
-		expirations: make(map[string]time.Time)
+		data:        make(map[string]string),
+		expirations: make(map[string]time.Time),
 	}
 }
